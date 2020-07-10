064cc2f72dedc2af3140941f0de6f6b8aee1882b
David Crandall <davecrands@gmail.com>
2020-07-10 14:15:02 -0500

    Add level three route

--- IntranetApp/ClientApp/src/App.js
+++ IntranetApp/ClientApp/src/App.js
@@ -4,8 +4,10 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { LevelThree } from './components/LevelThree';
 
 import './custom.css'
+
 
 export default class App extends Component {
   static displayName = App.name;
@@ -16,6 +18,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+        <Route path='/level-three' component={LevelThree} />
       </Layout>
     );
   }
